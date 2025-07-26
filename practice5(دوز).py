b7bceab55f1134955dfb40792f05ec3c7fc8f55e
AmiraliGhasemi7 <amirali464514@gmail.com>
2025-07-26 20:45:47 +0330

    dose

---
+++ practice5(دوز).py
@@ -0,0 +1,24 @@
+import random
+game=[1,2,3,4,5,6,7,8,9]
+robot=random.choice(game)
+print(robot)
+user=int(input("enter number one to nine: "))
+import random
+game=[1,2,3,4,5,6,7,8,9]
+robot=random.choice(game)
+print(robot)
+user=int(input("enter number one to nine: "))
+import random
+game=[1,2,3,4,5,6,7,8,9]
+robot=random.choice(game)
+print(robot)
+user=int(input("enter number one to nine: "))
+
+if robot==1 and 2 and 3 or 4 and 5 and 6 or 7 and 8 and 9 or 1 and 4 and 7 or 2 and 5 and 8 or 3 and 6 and 9 or 1 and 5 and 9 or 3 and 5 and 7:
+    print("robotwin")
+elif robot==1 and 2 and 3 or 4 and 5 and 6 or 7 and 8 and 9 or 1 and 4 and 7 or 2 and 5 and 8 or 3 and 6 and 9 or 1 and 5 and 9 or 3 and 5 and 7:
+    print("robotwin")
+elif robot==1 and 2 and 3 or 4 and 5 and 6 or 7 and 8 and 9 or 1 and 4 and 7 or 2 and 5 and 8 or 3 and 6 and 9 or 1 and 5 and 9 or 3 and 5 and 7:
+    print("robotwin")
+elif user== 1 and 2 and 3 or 4 and 5 and 6 or 7 and 8 and 9 or 1 and 4 and 7 or 2 and 5 and 8 or 3 and 6 and 9 or 1 and 5 and 9 or 3 and 5 and 7:
+    print("userwin")
